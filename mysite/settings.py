5bc11cbce89240a2bda7463283d48320a8f943ff
Sharif Ullah <sharif.ullah@bjitgroup.com>
2022-02-23 13:31:17 +0600

    fixed

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import  django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -157,3 +157,6 @@ MEDIA_ROOT = 'media/'
 #
 # MEDIA_ROOT is the physical path to the images.
 # MEDIA_URL is the URL path you use to access the media files
+
+
+django_heroku.settings(locals())
